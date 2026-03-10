f03a1a361106366ab3a3025cb7881be8b58c6060
Jose <jose.arias.lombardero@gmail.com>
2026-03-10 14:28:24 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /**
  * App - Componente principal de la Calculadora LCSP
- * Diputació de Castelló
+ * Concello de Lugo
  */
 
 import { useState } from 'react';
@@ -87,6 +87,8 @@ function App() {
                                     <li><strong>{t('daysType.naturales')}:</strong> {t('ruleNaturales')}</li>
                                     <li><strong>{t('ruleInhabil').split(':')[0]}:</strong> {t('ruleInhabil')}</li>
                                     <li><strong>{t('holidays')}:</strong> {t('ruleHolidays')}</li>
+                                    <li><strong>{t('daysType.habiles')}:</strong> {t('ruleHabiles')}</li>
+                                    <li><strong>{t('ruleFechaAFechaTitle')}:</strong> {t('ruleFechaAFecha')}</li>
                                 </ul>
                             </div>
                         </div>
